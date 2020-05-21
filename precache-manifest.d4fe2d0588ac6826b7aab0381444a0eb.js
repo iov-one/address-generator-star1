28ca3e44dcd16743f0c31920b900c3692ad3c25a
Dave Puchyr <dave@slim.avaritia.com>
2020-05-21 16:57:02 +0200

    Initial gh-pages cut.

---
+++ precache-manifest.d4fe2d0588ac6826b7aab0381444a0eb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c4f3d69203db4754733140b376859df",
+    "url": "/address-generator-star1/index.html"
+  },
+  {
+    "revision": "74516c0aea049549f691",
+    "url": "/address-generator-star1/static/css/2.4d454254.chunk.css"
+  },
+  {
+    "revision": "5e906f0ffc385529f406",
+    "url": "/address-generator-star1/static/css/main.3fbfedba.chunk.css"
+  },
+  {
+    "revision": "74516c0aea049549f691",
+    "url": "/address-generator-star1/static/js/2.edf9c51c.chunk.js"
+  },
+  {
+    "revision": "10da9032ca40e9b68c48f0c53357baef",
+    "url": "/address-generator-star1/static/js/2.edf9c51c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e906f0ffc385529f406",
+    "url": "/address-generator-star1/static/js/main.583c3639.chunk.js"
+  },
+  {
+    "revision": "21707e239fd74467cbfd",
+    "url": "/address-generator-star1/static/js/runtime-main.42fb90dc.js"
+  },
+  {
+    "revision": "ee12fc6d38d7e7f52a3b562de5b5a353",
+    "url": "/address-generator-star1/static/media/iov-logo.ee12fc6d.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/address-generator-star1/precache-manifest.d4fe2d0588ac6826b7aab0381444a0eb.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/address-generator-star1/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
